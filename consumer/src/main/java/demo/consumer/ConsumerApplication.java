85be269aff9a1e1a7eaaf86ac5877be0c41ce966
Gustavo Orsi <gustavoorsi@gmail.com>
2015-06-11 13:01:01 -0300

    Added MappingJackson2MessageConverter to read incoming messages from a queue.

--- consumer/src/main/java/demo/consumer/ConsumerApplication.java
+++ consumer/src/main/java/demo/consumer/ConsumerApplication.java
@@ -2,47 +2,64 @@ package demo.consumer;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 @EnableRabbit
-public class ConsumerApplication implements CommandLineRunner {
+public class ConsumerApplication implements RabbitListenerConfigurer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
 
-    @Autowired
-    private Receiver receiver;
+	@Bean
+	public MappingJackson2MessageConverter jackson2Converter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		return converter;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Bean
+	public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+		DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+		factory.setMessageConverter(jackson2Converter());
+		return factory;
+	}
 
-    }
+	@Override
+	public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+		registrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
+	}
+
+	@Autowired
+	private Receiver receiver;
 
 }
 
 @Service
 class Receiver {
-    @RabbitListener(queues = "queue")
-    public void receiveMessage(Foo foo) {
-        System.out.println("Received <" + foo.name + ">");
-    }
+	@RabbitListener(queues = "queue")
+	public void receiveMessage(Foo foo) {
+		System.out.println("Received <" + foo.name + ">");
+	}
 
-    @RabbitListener(queues = "queue")
-    public void receiveMessage(Bar bar) {
-        System.out.println("Received <" + bar.age + ">");
-    }
+	@RabbitListener(queues = "queue")
+	public void receiveMessage(Bar bar) {
+		System.out.println("Received <" + bar.age + ">");
+	}
 }
 
 class Foo {
-    public String name;
+	public String name;
 }
 
 class Bar {
-    public int age;
+	public int age;
 }
